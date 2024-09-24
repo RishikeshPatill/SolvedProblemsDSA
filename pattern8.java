271e0eb47a185d079b4aa99a50c2a26be87916bb
RishikeshPatill <patilrishi410@gmail.com>
2024-09-24 18:51:53 +0530

    pattern8 solved

---
+++ pattern8.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public static void nStarTriangle(int n) {
+      for(int i=n;i>0;i--){
+          for(int j=1;j<=n-i;j++){
+              System.out.print(" ");
+          }
+          for(int j=1;j<=2*i-1;j++){
+              System.out.print("*");
+          }
+          System.out.println();
+      }
+    }
+}
