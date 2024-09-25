8982d9d1791c5c5b7b947062f81a0f2b5f439bca
RishikeshPatill <patilrishi410@gmail.com>
2024-09-25 10:32:05 +0530

    pattern10 striver sheet

---
+++ pattern10.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public static void nStarTriangle(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<=n-1;i++){
+            for(int j=n-1;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
