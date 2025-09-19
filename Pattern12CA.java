efa265ec8b4dc9bd7a5bf13c5185f8605dc6d063
RishikeshPatill <patilrishi410@gmail.com>
2025-09-19 21:52:31 +0530

    done

---
+++ Pattern12CA.java
@@ -0,0 +1,19 @@
+package striverDsaSheet;
+import java.util.Scanner;
+public class Pattern12CA {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the value of rows:");
+		int row=sc.nextInt();
+		System.out.println("Pattern 10 is here");
+		System.out.println("-----------------");
+		for(int i=1;i<=row;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(i+" ");
+			
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+}
