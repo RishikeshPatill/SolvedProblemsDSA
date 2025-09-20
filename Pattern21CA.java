581200af0c9b896d94c26ebc43906f8b25570196
RishikeshPatill <patilrishi410@gmail.com>
2025-09-20 20:33:21 +0530

    done

---
+++ Pattern21CA.java
@@ -0,0 +1,22 @@
+package striverDsaSheet;
+import java.util.Scanner;
+public class Pattern21CA {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number of rows:");
+		int num=sc.nextInt();
+		System.out.println("Pattern 20 is here");
+		System.out.println("------------------");
+		for(int i=1;i<=num;i++) {
+			for(int j=1;j<=num-i;j++) {
+				System.out.print(" "+" ");
+			}
+			for(int m=1;m<=i;m++) {
+				char ch=(char)('a'+m-1);
+				System.out.print(ch+" ");
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+}
