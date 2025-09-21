ae2203f2b71495706f96f83d9f966fd87ea18b74
RishikeshPatill <patilrishi410@gmail.com>
2025-09-21 22:28:41 +0530

    done

---
+++ Pattern27CA.java
@@ -0,0 +1,36 @@
+package striverDsaSheet;
+import java.util.Scanner;
+public class Pattern27CA {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number of rows:");
+		int num=sc.nextInt();
+		System.out.println("Pattern 27 is here");
+		System.out.println("------------------");
+		for(int i=1;i<=num;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*"+" ");
+			}
+			for(int m=1;m<=2*(num-i);m++) {
+				System.out.print(" "+" ");
+			}
+			for(int n=1;n<=i;n++) {
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}
+		for(int i=num-1;i>=1;i--) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*"+" ");
+			}
+			for(int m=1;m<=2*(num-i);m++) {
+				System.out.print(" "+" ");
+			}
+			for(int n=1;n<=i;n++) {
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+}
